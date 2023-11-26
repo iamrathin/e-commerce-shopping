7f25cdb6235f30f1382527cfc052fbcdc25d18ae
iamrathin <iamrathinsharma@gmail.com>
2023-11-27 00:36:06 +0530

    cart page added

---
+++ frontend/src/features/cart/cartAPI.js
@@ -0,0 +1,8 @@
+// A mock function to mimic making an async request for data
+export function fetchCart(amount = 1) {
+  return new Promise(async (resolve) => {
+    const resposne = await fetch("http://localhost:8080");
+    const data = await resposne.json();
+    resolve({ data });
+  });
+}
